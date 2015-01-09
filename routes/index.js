fd853b7929b57aedb2c9d96d1b12976552f94c31
Liorr Yehuda Morrison <liorr.morrison@gmail.com>
2015-01-08 23:40:33 -0800

    arabized my first express app

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'العربية' , name: 'ليور يهوده موريسون'});
 });
 
 module.exports = router;
