3f009345bc3702c40838f495e5ba8ab065cccfa0
Liorr Yehuda Morrison <liorr.morrison@gmail.com>
2015-01-09 23:04:06 -0800

    fleshes out controller routeto test how this beast works

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res) {
-  res.send('respond with a resource');
+router.get('/arabic', function(req, res) {
+  var arabicAlpha = ['ا', 'ب', 'ت','ث', 'ج', 'ح', 'خ',  'د', 'ذ', 'ر', 'ز', 'س', 'ش', 'ص', 'ض', 'ط', 'ظ', 'ع', 'غ', 'ف', 'ق', 'ك', 'ل', 'م', 'ن', 'ه', 'و', 'ي' ]
+
+  res.send(arabicAlpha);
 });
 
 module.exports = router;
