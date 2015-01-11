7f0bdd17f86aac2c7ea63005c04404368566f42b
Liorr Yehuda Morrison <liorr.morrison@gmail.com>
2015-01-10 23:12:06 -0800

    changed route and view to accommodate arabic alphabet array

--- routes/users.js
+++ routes/users.js
@@ -4,8 +4,7 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/arabic', function(req, res) {
   var arabicAlpha = ['ا', 'ب', 'ت','ث', 'ج', 'ح', 'خ',  'د', 'ذ', 'ر', 'ز', 'س', 'ش', 'ص', 'ض', 'ط', 'ظ', 'ع', 'غ', 'ف', 'ق', 'ك', 'ل', 'م', 'ن', 'ه', 'و', 'ي' ]
-
-  res.send(arabicAlpha);
+  res.render('arabic', {arabic: arabicAlpha})
 });
 
 module.exports = router;
