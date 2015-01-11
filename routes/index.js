13e2f3221a135bd9f84c6ab9194ad43e40f904e7
Liorr Yehuda Morrison <liorr.morrison@gmail.com>
2015-01-11 00:04:40 -0800

    finally getting second route to work

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'العربية' , name: 'ليور يهوده موريسون'});
 });
 
+router.get('/arabic', function(req, res) {
+  arabicAlpha = ['ا', 'ب', 'ت','ث', 'ج', 'ح', 'خ',  'د', 'ذ', 'ر', 'ز', 'س', 'ش', 'ص', 'ض', 'ط', 'ظ', 'ع', 'غ', 'ف', 'ق', 'ك', 'ل', 'م', 'ن', 'ه', 'و', 'ي' ]
+  res.render('arabic', { arabic: arabicAlpha });
+});
+
+
+
 module.exports = router;
